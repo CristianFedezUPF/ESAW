1dacc4dcaaa86129029e98d5d1c01441eeda850b
CristianFedezUPF <72511232+CristianFedezUPF@users.noreply.github.com>
2022-06-07 20:55:53 +0200

    query fix

--- P4/src/main/resources/sql/query.sql
+++ P4/src/main/resources/sql/query.sql
@@ -7,4 +7,4 @@ SELECT * FROM tweet;
 #currently the user that follows more people is cristian (with id = 3) so let's search 
 #	for the tweets these people have posted
 
-SELECT content FROM tweet WHERE user_id = ANY (SELECT followed_id FROM `follows` WHERE follower_id = 3);
+SELECT content FROM tweet WHERE user_id IN (SELECT followed_id FROM `follows` WHERE follower_id = 3);
